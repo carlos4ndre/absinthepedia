922bd4ec5bb8321968998cdf2bf388773c3f17bd
Carlos Andre <carlos@revl.world>
2017-06-07 09:22:04 +0100

    Fix schema definition

--- web/schema.ex
+++ web/schema.ex
@@ -2,24 +2,20 @@ defmodule Absinthepedia.Schema do
   use Absinthe.Schema
   import_types Absinthepedia.Schema.Types
 
-  @desc "Get all recipes"
   query do
+    @desc "Get all recipes"
     field :recipes, list_of(:recipe) do
       resolve &Absinthepedia.RecipeResolver.all/2
     end
-  end
 
-  @desc "Get all ingredients"
-  query do
+    @desc "Get all ingredients"
     field :ingredients, list_of(:ingredient) do
       resolve &Absinthepedia.IngredientResolver.all/2
     end
-  end
 
-  @desc "Get all units"
-  query do
+    @desc "Get all units"
     field :units, list_of(:unit) do
-      resolve $Absinthepedia.UnitResolver.all/2
+      resolve &Absinthepedia.UnitResolver.all/2
     end
   end
 end
