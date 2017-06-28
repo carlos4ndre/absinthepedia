a9479d1ffc3f247cd91a652de16ed2b52570a873
Carlos Andre <carlos@revl.world>
2017-06-28 23:51:10 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Absinthepedia
 
-Alcoholic wiki built with Phoenix, GraphQL and React.
+Website built with Phoenix, GraphQL and React.
 
-🔥🔥🔥 DISCLAMER: This project is only for learning purposes, and it doesn't
-sponser any crazy party 🍺🍸🍹🍶😎
+🔥🔥🔥 DISCLAMER: This project is only for learning purposes, 
+and it doesn't pretend to sponser any crazy party.
 
 ## How to run the app?
 
 We use docker to spin up everything, go ahead and run this command:
+
+```
 $ docker-compose up --build
+```
--- priv/react/app/containers/RecipesPage/index.js
+++ priv/react/app/containers/RecipesPage/index.js
@@ -8,6 +8,7 @@ import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
 import H1 from 'components/H1';
+import H3 from 'components/H3';
 import messages from './messages';
 
 export default class RecipesPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -30,6 +31,9 @@ export default class RecipesPage extends React.Component { // eslint-disable-lin
         <H1>
           <FormattedMessage {...messages.header} />
         </H1>
+        <H3>
+          TODO: Use <i>relay</i> to fetch recipes!
+        </H3>
       </div>
     );
   }
