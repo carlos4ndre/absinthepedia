7ecee68dfc8b81badbcb569bc84bedce88c8ca34
Carlos André <carlos.ulrich@gmail.com>
2017-08-10 22:19:30 +0100

    Minor change to dockerfile

--- run_web.sh
+++ run_web.sh
@@ -3,14 +3,6 @@
 # wait for PSQL server to start
 sleep 10
 
-# build react app
-cd /code/priv/react
-yarn install && yarn build
-
-# install phoenix dependencies
-cd /code
-mix deps.get
-
 # migrate db, so we have the latest db schema
 mix ecto.setup
 
